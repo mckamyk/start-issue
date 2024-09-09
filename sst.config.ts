43fa8606657ceeb2b4e0a41e89bdfdbf899776e0
McKamyk <mckittrick.kaminski@gmail.com>
2024-09-09 11:55:33 -0500

    fixes?

--- sst.config.ts
+++ sst.config.ts
@@ -7,16 +7,18 @@ export default $config({
         if (e.action === 'pushed' && e.type === 'branch') {
           const stage = e.branch === 'main' ? 'prod' : e.branch
 
-          const webhook = process.env.WEBHOOK!
-          fetch(webhook, {
-            method: 'post',
-            headers: {
-              'content-type': 'application/json',
-            },
-            body: JSON.stringify({
-              content: `Updating start on the \`${stage}\` stage.\n${e.commit.message}`,
-            }),
-          })
+          const webhook = process.env.WEBHOOK
+          if (!!webhook) {
+            fetch(webhook, {
+              method: 'post',
+              headers: {
+                'content-type': 'application/json',
+              },
+              body: JSON.stringify({
+                content: `Updating start on the \`${stage}\` stage.\n${e.commit.message}`,
+              }),
+            })
+          }
 
           return {
             stage,
@@ -35,8 +37,12 @@ export default $config({
   async run() {
     const secret = new sst.Secret('TestSecret')
 
-    new sst.aws.TanstackStart('MyWeb', {
+    const web = new sst.aws.TanstackStart('MyWeb', {
       link: [secret],
     })
+
+    return {
+      url: web.url,
+    }
   },
 })
