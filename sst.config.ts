a6454d2fe5627c842b2981656bbc5a9dc44bbef6
McKamyk <mckittrick.kaminski@gmail.com>
2024-09-09 12:16:33 -0500

    gh message

--- sst.config.ts
+++ sst.config.ts
@@ -1,7 +1,32 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
-const webhook =
-  'https://discord.com/api/webhooks/1282742099787251866/7VYfPWVON2tDT-rYAXcpdm445fjPoiF3bRzJIRhojgYEYN5VuPb7TP9X4ljRWp_E1ta2'
+const postMessage = async (
+  stage: string,
+  commitMessage: string,
+  commitHash: string,
+) => {
+  const webhook =
+    'https://discord.com/api/webhooks/1282742099787251866/7VYfPWVON2tDT-rYAXcpdm445fjPoiF3bRzJIRhojgYEYN5VuPb7TP9X4ljRWp_E1ta2'
+
+  const content: string[] = [
+    `## Updating **Start** on the ${stage} stage.`,
+    `Commit Message: ${commitMessage}`,
+    `Links:`,
+    `- [SST Console](https://console.sst.dev/foobarbinbaz/start/${stage}/resources)`,
+    `- [Deployment CICD](https://console.sst.dev/foobarbinbaz/start/${stage}/autodeploy)`,
+    `- [GH Commit](https://github.com/mckamyk/start-issue/commit/${commitHash})`,
+  ]
+
+  fetch(webhook, {
+    method: 'post',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      content: content.join('\n'),
+    }),
+  })
+}
 
 export default $config({
   console: {
@@ -10,16 +35,7 @@ export default $config({
         if (e.action === 'pushed' && e.type === 'branch') {
           const stage = e.branch === 'main' ? 'prod' : e.branch
 
-          fetch(webhook, {
-            method: 'post',
-            headers: {
-              'content-type': 'application/json',
-            },
-            body: JSON.stringify({
-              content: `Updating start on the \`${stage}\` stage.\n${e.commit.message}`,
-            }),
-          })
-
+          postMessage(stage, e.commit.message, e.commit.id)
           return {
             stage,
           }
